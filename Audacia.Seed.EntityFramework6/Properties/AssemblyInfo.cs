215acd2cf538aaafae8f8e947d3ef877e464ee1e
Adam Drewery <adam.drewery@audacia.co.uk>
2019-07-10 15:17:43 +0100

    Nuget settings.

--- Audacia.Seed.EntityFramework6/Properties/AssemblyInfo.cs
+++ Audacia.Seed.EntityFramework6/Properties/AssemblyInfo.cs
@@ -5,13 +5,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Audacia.Seed.EntityFramework6")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Entity Framework 6 integration for Audacia.Seed")]
+[assembly: AssemblyCompany("Audacia")]
 [assembly: AssemblyProduct("Audacia.Seed.EntityFramework6")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Audacia 2019")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
